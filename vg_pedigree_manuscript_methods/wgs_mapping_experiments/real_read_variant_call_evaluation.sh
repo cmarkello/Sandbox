f3ab259c9053d916305aa6e0c564d39f43d8a72a
Charles Markello <cmarkell@ucsc.edu>
2021-10-02 23:33:20 -0400

    Finished 1st draft script for variant calling evals for vg pedigree manuscript

--- vg_pedigree_manuscript_methods/wgs_mapping_experiments/real_read_variant_call_evaluation.sh
+++ vg_pedigree_manuscript_methods/wgs_mapping_experiments/real_read_variant_call_evaluation.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# real_read_deepvariant_call.sh: indel realign input BAMs and run the DeepVariant Genotyper
+# real_read_variant_call_evaluation.sh: Run happy and rtg vcfevals on input experiment VCF variant called file against various regions.
 
 set -ex
 set -o pipefail
@@ -36,7 +36,68 @@ function make_bedfile() {
     fi
 }
 
-USE_DEFAULT_MODEL=true
+function make_nosnp1kg_bedfile() {
+    if [ ! -e "${3}" ] ; then
+        docker run \
+        -e HIGH_CONF_BED=${1} \
+        -e SNP1KG_VCF_SITES_FILE=${2} \
+        -e HIGH_CONF_NOSNP1KG_BED=${3} \
+        -v ${PWD}:${HOME} -w ${HOME} quay.io/biocontainers/bedtools:2.27.0--1
+        bedtools subtract \
+        -a ${HIGH_CONF_BED} \
+        -b ${SNP1KG_VCF_SITES_FILE} \
+        > ${HIGH_CONF_NOSNP1KG_BED}
+    fi
+}
+
+function run_happy() {
+    if [ ! -d "${1}" ] ; then
+        OUT_DIR=${1}
+        TRUTH_VCF=${2}
+        TRUTH_BED=${3}
+        CALLED_VCF=${4}
+        REF_FASTA=${5}
+        docker run \
+        -e OUT_DIR=${OUT_DIR} \
+        -e TRUTH_VCF=${TRUTH_VCF} \
+        -e TRUTH_BED=${TRUTH_BED} \
+        -e CALLED_VCF=${CALLED_VCF} \
+        -e REF_FASTA=${REF_FASTA} \
+        -v ${PWD}:${HOME} -w ${HOME} jmcdani20/hap.py:v0.3.12 \
+            /opt/hap.py/bin/hap.py \
+            ${TRUTH_VCF} \
+            ${CALLED_VCF} \
+            -f ${TRUTH_BED} \
+            --reference ${REF_FASTA} \
+            --threads 16 \
+            --engine=vcfeval \
+            -o ${OUT_DIR}/happy_output
+    fi
+}
+
+function run_rtgvcfeval() {
+    if [ ! -d "${1}" ] ; then
+        OUT_DIR=${1}
+        TRUTH_VCF=${2}
+        TRUTH_BED=${3}
+        CALLED_VCF=${4}
+        REF_FASTA=${5}
+        docker run \
+        -e TRUTH_VCF=${TRUTH_VCF} \
+        -e TRUTH_BED=${TRUTH_BED} \
+        -e CALLED_VCF=${CALLED_VCF} \
+        -e REF_FASTA=${REF_FASTA} \
+        -v ${PWD}:${HOME} -w ${HOME} realtimegenomics/rtg-tools:3.8.4 \
+            rtg vcfeval \
+            --baseline ${TRUTH_VCF} \
+            --calls ${CALLED_VCF} \
+            --evaluation-regions ${TRUTH_BED} \
+            --output ${OUT_DIR} \
+            --template ${REF_FASTA}.sdf \
+            --threads 16
+    fi
+}
+
 WORKDIR=${HOME}/run_deeptrio_genotyping
 CHILD_NAME="${1}"
 VCF_FILE_CHILD="${2}"
@@ -73,104 +134,36 @@ docker run \
     ${REF_FASTA} \
     -o ${REF_FASTA}.sdf
 
-make_bedfile ${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed GRCh38_alldifficultregions.bed.gz ${CHILD_NAME}_GRCh38_v4.2.1.all_difficult_regions.bed
-make_bedfile ${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed GRCh38_alldifficultregions.bed.gz ${CHILD_NAME}_GRCh38_v4.2.1.all_difficult_regions.bed
+# Evaluate Called File in All High Confident Regions
+make_nosnp1kg_bedfile ${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed ALL.GRCh38.genotypes.20170504.no_segdups_gt10kb.sites.sorted.vcf.gz ${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.NO_SNP1KG.specific.bed
+run_happy happy_vcfeval_output_${CHILD_NAME}_allhighconfregions "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+run_happy happy_vcfeval_output_${CHILD_NAME}_allhighconfregions_NOSNP1KG "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.NO_SNP1KG.specific.bed" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+run_rtgvcfeval rtg_vcfeval_output_${CHILD_NAME}_allhighconfregions "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+run_rtgvcfeval rtg_vcfeval_output_${CHILD_NAME}_allhighconfregions_NOSNP1KG "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.NO_SNP1KG.specific.bed" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
 
+# Evaluate Called File in Difficult Regions
+for REGION in "MHC.bed" "alllowmapandsegdupregions.bed" "alldifficultregions.bed" ; do
+    cd $WORKDIR
+    make_bedfile ${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed GRCh38_${REGION}.gz ${CHILD_NAME}_GRCh38_v4.2.1.${REGION}
+    make_bedfile ${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.NO_SNP1KG.specific.bed GRCh38_${REGION}.gz ${CHILD_NAME}_GRCh38_v4.2.1.NO_SNP1KG.specific.${REGION}
 
-# Evaluate Called File
-for REGION in "HG002_GRCh38_CMRG_smallvar_v1.00.bed" "HG002_GRCh38_v4.2.1.MHC.bed" "HG002_GRCh38_v4.2.1.alllowmapandsegdupregions.bed" "HG002_GRCh38_v4.2.1.all_difficult_regions.bed" "HG002_GRCh38_CHROM1-22_v4.2.1.highconf.NO_SNP1KG.specific.bed" "HG002_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed" ; do
-cd $WORKDIR
-docker run \
--e TRUTH_VCF=${TRUTH_VCF} \
--e TRUTH_BED=${TRUTH_BED} \
--e CALLED_VCF=${CALLED_VCF} \
--e REF_FASTA=${REF_FASTA} \
--v ${PWD}:${HOME} -w ${HOME} quay.io/biocontainers/bedtools:2.27.0--1
-    bedtools intersect \
-    -a /data/markellocj/benchmark_data/HG002_cohort/HG002_GRCh38_CMRG_smallvar_v1.00.bed \
-    -b /data/markellocj/benchmark_data/HG002_cohort/HG002_GRCh38_CHROM1-22_v4.2.1.highconf.NO_SNP1KG.specific.bed \
-    > HG002_GRCh38_v4.2.1.NO_SNP1KG.specific.CMRG.bed
-
-rm -r $WORKDIR/happy_vcfeval_output_${CHILD_NAME}_${REGION}
-TRUTH_VCF="${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" \
-TRUTH_BED="${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed" \
-CALLED_VCF="${CHILD_NAME}.vcf.gz" \
-docker run \
--e TRUTH_VCF=${TRUTH_VCF} \
--e TRUTH_BED=${TRUTH_BED} \
--e CALLED_VCF=${CALLED_VCF} \
--e REF_FASTA=${REF_FASTA} \
--v ${PWD}:${HOME} -w ${HOME} jmcdani20/hap.py:v0.3.12 \
-    /opt/hap.py/bin/hap.py \
-    ${TRUTH_VCF} \
-    ${CALLED_VCF} \
-    -f ${TRUTH_BED} \
-    --reference ${REF_FASTA} \
-    --threads 16 \
-    --engine=vcfeval \
-    -o happy_vcfeval_output_${CHILD_NAME}_${REGION}
-
-rm -r $WORKDIR/rtg_vcfeval_output_${CHILD_NAME}_${REGION}
-docker run \
--e TRUTH_VCF=${TRUTH_VCF} \
--e TRUTH_BED=${TRUTH_BED} \
--e CALLED_VCF=${CALLED_VCF} \
--e REF_FASTA=${REF_FASTA} \
--e OUT_NAME=rtg_vcfeval_output_${CHILD_NAME}_${REGION} \
--v ${PWD}:${HOME} -w ${HOME} realtimegenomics/rtg-tools:3.8.4 \
-    rtg vcfeval \
-    --baseline ${TRUTH_VCF} \
-    --calls ${CALLED_VCF} \
-    --evaluation-regions ${TRUTH_BED} \
-    --output ${OUT_NAME} \
-    --template ${REF_FASTA}.sdf \
-    --threads 16
+    run_happy happy_vcfeval_output_${CHILD_NAME}_${REGION} "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+    run_happy happy_vcfeval_output_${CHILD_NAME}_${REGION}_NOSNP1KG "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.NO_SNP1KG.specific.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+    run_rtgvcfeval rtg_vcfeval_output_${CHILD_NAME}_${REGION} "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+    run_rtgvcfeval rtg_vcfeval_output_${CHILD_NAME}_${REGION}_NOSNP1KG "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.NO_SNP1KG.specific.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+done
 
 if [[ ${CHILD_NAME} == *"HG002"* ]]; then
-    REGION="CMRG"
     wget_download https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG002_NA24385_son/CMRG_v1.00/GRCh38/SmallVariant/HG002_GRCh38_CMRG_smallvar_v1.00.vcf.gz "${WORKDIR}/${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.CMRG.vcf.gz"
     wget_download https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG002_NA24385_son/CMRG_v1.00/GRCh38/SmallVariant/HG002_GRCh38_CMRG_smallvar_v1.00.vcf.gz.tbi "${WORKDIR}/${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.CMRG.vcf.gz.tbi"
-    wget_download https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG002_NA24385_son/CMRG_v1.00/GRCh38/SmallVariant/HG002_GRCh38_CMRG_smallvar_v1.00.bed "${WORKDIR}/${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.CMRG.bed"
+    wget_download https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/release/AshkenazimTrio/HG002_NA24385_son/CMRG_v1.00/GRCh38/SmallVariant/HG002_GRCh38_CMRG_smallvar_v1.00.bed "${WORKDIR}/${CHILD_NAME}_GRCh38_v4.2.1.CMRG.bed"
+    REGION="CMRG.bed"
     cd $WORKDIR
-    rm -r $WORKDIR/happy_vcfeval_output_${CHILD_NAME}_${REGION}
-    TRUTH_VCF="${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.vcf.gz" \
-    TRUTH_BED="${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark_noinconsistent.bed" \
-    CALLED_VCF="${CHILD_NAME}.vcf.gz" \
-    docker run \
-    -e TRUTH_VCF=${TRUTH_VCF} \
-    -e TRUTH_BED=${TRUTH_BED} \
-    -e CALLED_VCF=${CALLED_VCF} \
-    -e REF_FASTA=${REF_FASTA} \
-    -v ${PWD}:${HOME} -w ${HOME} jmcdani20/hap.py:v0.3.12 \
-        /opt/hap.py/bin/hap.py \
-        ${TRUTH_VCF} \
-        ${CALLED_VCF} \
-        -f ${TRUTH_BED} \
-        --reference ${REF_FASTA} \
-        --threads 16 \
-        --engine=vcfeval \
-        -o happy_vcfeval_output_${CHILD_NAME}_${REGION}
-
-    rm -r $WORKDIR/rtg_vcfeval_output_${CHILD_NAME}_${REGION}
-    docker run \
-    -e TRUTH_VCF=${TRUTH_VCF} \
-    -e TRUTH_BED=${TRUTH_BED} \
-    -e CALLED_VCF=${CALLED_VCF} \
-    -e REF_FASTA=${REF_FASTA} \
-    -e OUT_NAME=rtg_vcfeval_output_${CHILD_NAME}_${REGION} \
-    -v ${PWD}:${HOME} -w ${HOME} realtimegenomics/rtg-tools:3.8.4 \
-        rtg vcfeval \
-        --baseline ${TRUTH_VCF} \
-        --calls ${CALLED_VCF} \
-        --evaluation-regions ${TRUTH_BED} \
-        --output ${OUT_NAME} \
-        --template ${REF_FASTA}.sdf \
-        --threads 16
-    
-    # TODO
-    cd /data/markellocj/benchmark_data/HG002_cohort
-    bedtools intersect \
-    -a /data/markellocj/benchmark_data/HG002_cohort/HG002_GRCh38_CMRG_smallvar_v1.00.bed \
-    -b /data/markellocj/benchmark_data/HG002_cohort/HG002_GRCh38_CHROM1-22_v4.2.1.highconf.NO_SNP1KG.specific.bed \
-    > HG002_GRCh38_v4.2.1.NO_SNP1KG.specific.CMRG.bed
+    make_nosnp1kg_bedfile ${CHILD_NAME}_GRCh38_v4.2.1.${REGION} ALL.GRCh38.genotypes.20170504.no_segdups_gt10kb.sites.sorted.vcf.gz ${CHILD_NAME}_GRCh38_v4.2.1.NO_SNP1KG.specific.${REGION}
+    run_happy happy_vcfeval_output_${CHILD_NAME}_allhighconfregions "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.CMRG.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+    run_happy happy_vcfeval_output_${CHILD_NAME}_allhighconfregions_NOSNP1KG "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.CMRG.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.NO_SNP1KG.specific.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+    run_rtgvcfeval rtg_vcfeval_output_${CHILD_NAME}_allhighconfregions "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.CMRG.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
+    run_rtgvcfeval rtg_vcfeval_output_${CHILD_NAME}_allhighconfregions_NOSNP1KG "${CHILD_NAME}_GRCh38_1_22_v4.2.1_benchmark.CMRG.vcf.gz" "${CHILD_NAME}_GRCh38_v4.2.1.NO_SNP1KG.specific.${REGION}" "${CHILD_NAME}.vcf.gz" ${REF_FASTA}
 fi
+
+
