73acd64e918e02a1831e8ec16e849c5d40b9149a
Charles Markello <cmarkell@ucsc.edu>
2021-10-03 19:34:30 -0400

    Polished graph construction for vg pedigree manuscript

--- vg_pedigree_manuscript_methods/graph_construction/make_1000GPlons_GRCh38_graph.sh
+++ vg_pedigree_manuscript_methods/graph_construction/make_1000GPlons_GRCh38_graph.sh
@@ -1,47 +1,130 @@
 #!/usr/bin/env bash
-# If rerunning, the Toil 6 and vg 1.33 releases are probably the right choices.
-# Run in screen on a Kubernetes cluster: TOIL_APPLIANCE_SELF=quay.io/ucsc_cgl/toil:5.4.0a1-1530a9190357fc058333f3e929049ef9593a6784-py3.7 toil launch-cluster --provisioner aws -T kubernetes -z us-west-2a adamnovak-toil-vg --leaderNodeType t3a.medium --nodeTypes=t3a.medium,r5ad.24xlarge,r5d.24xlarge/r5ad.24xlarge:2.50,i3.8xlarge:1.50 --workers 1-4,0-1,0-8,0-6 --keyPairName anovak@soe.ucsc.edu
-# Doesn't build the GCSA indexes for vg map.
-# Does build the sample graph positive control.
-# When rerunning, use --defaultPreemptable and/or throw max 5 of the on-demand workers at it
+# make_1000GPlons_GRCh38_graph.sh: Main GRCh38-based graph construction workflow
 
-rm -Rf /tmp/work  
-mkdir /tmp/work
-cd /tmp/work
+set -ex
+set -o pipefail
+
+function download() {
+    if [ ! -e "${2}" ] ; then
+        aws s3 cp --no-progress "${1}" "${2}"
+    fi
+}
+
+function wget_download() {
+    if [ ! -e "${2}" ] ; then
+        wget_download "${1}" -O "${2}"
+    fi
+}
+
+function copy() {
+    if [ ! -e "${2}" ] ; then
+        cp "${1}" "${2}"
+    fi
+}
+
+WORKDIR=${HOME}/run_graph_construction
+WORKFLOW_INPUT_DIR=${WORKDIR}/inputs
+
+# Where should temp files go?
+mkdir -p "${WORKDIR}" "${WORKFLOW_INPUT_DIR}"
+export TMPDIR="${WORKDIR}/tmp"
+mkdir -p "${TMPDIR}"
+
+# Download data input data
+cd $WORKDIR
+wget_download https://storage.googleapis.com/cmarkell-vg-wdl-dev/grch38_inputs/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.compact_decoys.fna.gz -O ${WORKFLOW_INPUT_DIR}/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.compact_decoys.fna.gz
+
+# Setup toil-vg virtual environment
+cd $WORKDIR
+git clone --single-branch --branch vg_pedigree_workflow_deepvariant_grch38 https://github.com/vgteam/toil-vg.git
+git clone https://github.com/cmarkello/toil.git
+python3 -m venv toilvg_venv
 virtualenv --system-site-packages --python python3 venv
-. venv/bin/activate
-pip3 install --upgrade git+https://github.com/vgteam/toil-vg.git@b319a1b22df6dac585b7f95bc1a603577452d443#egg=toil-vg
+source venv/bin/activate
+pip3 install ./toil
+pip3 install ./toil-vg
 toil-vg generate-config --whole_genome >config.cfg
 sed -i'' config.cfg -e "s/gcsa-index-mem: '110G'/gcsa-index-mem: '700G'/g" -e "s/gcsa-index-disk: '2200G'/gcsa-index-disk: '4096G'/g" -e "s/construct-mem: '64G'/construct-mem: '128G'/g" -e "s/construct-disk: '64G'/construct-disk: '128G'/g" -e "s/gbwt-index-mem: '35G'/gbwt-index-mem: '70G'/g" -e "s/gbwt-index-disk: '100G'/gbwt-index-disk: '200G'/g"
-toil clean aws:us-west-2:adamnovak-make-1000gplons-graphs
+
+# Setup construction workflow
+export XDG_RUNTIME_DIR=""
+CONST_SOURCE_VCFS="/data/markellocj/graph_reference/construction_source_files/1kg_phased_genotypes_grch38/liftover_from_grch37_no_segdubs_gt10kb"
 # VCF order must be 1-22, X, Y to match FASTA
-VCFS=()
-for CHROM in {1..22} X Y ; do
-  VCFS+=("s3://vg-data/1kg_GRCh38/variants/subsets/no_segdups_gt10kb/ALL.chr${CHROM}_GRCh38.genotypes.20170504.no_segdups_gt10kb.vcf.gz")
-done
-export TOIL_KUBERNETES_HOST_PATH=/var/lib/toil
-export SINGULARITY_CACHEDIR=/var/lib/toil/singularity-cache
+CONST_VCFS=()
+CONST_VCFS=($((seq 1 22) | xargs -I {} echo ${CONST_SOURCE_VCFS}/ALL.chr{}_GRCh38.genotypes.20170504.no_segdups_gt10kb.renamed.vcf.gz))
+CONST_VCFS+=(${CONST_SOURCE_VCFS}/ALL.chrX_GRCh38.genotypes.20170504.no_segdups_gt10kb.renamed.vcf.gz)
+CONST_VCFS+=(${CONST_SOURCE_VCFS}/ALL.chrY_GRCh38.genotypes.20170504.no_segdups_gt10kb.renamed.vcf.gz)
+CONST_VCFS_PHASED=()
+CONST_VCFS_PHASED=($((seq 1 22) | xargs -I {} echo ${CONST_SOURCE_VCFS}/CCDG_14151_B01_GRM_WGS_2020-08-05_chr{}.filtered.shapeit2-duohmm-phased.vcf.gz))
+CONST_VCFS_PHASED+=(${CONST_SOURCE_VCFS}/ALL.chrX_GRCh38.genotypes.20170504.no_segdups_gt10kb.renamed.vcf.gz)
+TOILVG_OUTSTORE="${WORKDIR}/construct-grch38-fast-liftover-nosegdup-outstore"
+TOILVG_JOBSTORE="${WORKDIR}/construct-grch38-fast-liftover-nosegdup-jobstore"
+LOGFILE="${WORKDIR}/construct_grch38_fast_liftover_nosegdup.log"
+mkdir -p ${TOILVG_OUTSTORE}
+rm -f ${LOGFILE}
+export TOIL_SLURM_ARGS="-t 40:00:00"
+export SINGULARITY_CACHEDIR=${WORKDIR}/singularity-cache
+
+# Run the construction workflow
+toil clean ${TOILVG_JOBSTORE}
 toil-vg construct \
-  aws:us-west-2:make-1000gplons-graphs \
-  aws:us-west-2:vg-k8s/profiling/graphs/v3/for-NA19239/1000gplons/hs38d1 \
-  --fasta s3://vg-k8s/profiling/data/GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.renamed.fna.gz \
-  --coalesce_regions s3://vg-k8s/profiling/data/GCA_000001405.15_GRCh38_no_alt_analysis_set_plus_GCA_000786075.2_hs38d1_genomic.minor_contigs.tsv \
-  --vcf "${VCFS[@]}" \
-  --vcf_phasing "${VCFS[@]}" \
-  --fasta_regions \
-  --alt_paths \
-  --out_name 1000GPlons_hs38d1 \
-  --xg_index --gbwt_index --trivial_snarls_index --distance_index \
-  --force_phasing True \
-  --filter_samples NA19239 NA19240 \
-  --pangenome \
-  --merge_graphs \
-  --keep_vcfs \
-  --config config.cfg \
-  --realTimeLogging \
-  --logInfo \
-  --batchSystem kubernetes \
-  --disableCaching false \
-  --vg_docker quay.io/vgteam/vg:ci-2890-655a9622c3d60e87f14b88d943fbd8554214a975 \
-  --container Singularity \
-  --defaultDisk 400G --defaultMemory 400G 2>&1 | tee -a log.txt
+${TOILVG_JOBSTORE} \
+${TOILVG_OUTSTORE} \
+--batchSystem slurm \
+--container Singularity \
+--logInfo \
+--vg_docker 'quay.io/vgteam/vg:ci-2351-a64b70c1f9345f0821e3f3a600eb8bbf4fe44bf2' \
+--workDir ${WORKDIR}/tmp \
+--cleanWorkDir onSuccess \
+--config config.cfg \
+--fasta ${WORKFLOW_INPUT_DIR}/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.compact_decoys.fna.gz \
+--out_name liftover_snp1kg_grch38_nosegdup \
+--logFile ${LOGFILE} \
+--xg_index --gbwt_index --trivial_snarls_index \
+--force_phasing True \
+--merge_graphs \
+--fasta_regions \
+--pangenome \
+--vcf ${CONST_VCFS[@]} \
+--vcf_phasing ${CONST_VCFS_PHASED[@]} \
+--statePollingWait 120 \
+--rescueJobsFrequency 120 \
+--defaultMemory 50Gi --defaultDisk 400G
+
+# Build sampled GBWT-based indexes
+cd $WORKDIR/construct-grch38-fast-liftover-nosegdup-outstore
+docker run \
+-v ${PWD}:${HOME} -w ${HOME} quay.io/vgteam/vg:ci-2351-a64b70c1f9345f0821e3f3a600eb8bbf4fe44bf2 \
+    vg index \
+    -s liftover_snp1kg_grch38_nosegdup.trivial.snarls \
+    -j liftover_snp1kg_grch38_nosegdup.trivial.snarls.dist \
+    -x liftover_snp1kg_grch38_nosegdup.xg \
+    -t 32 -p
+
+# Build sampled gbwt and gbwt graph
+docker run \
+-v ${PWD}:${HOME} -w ${HOME} quay.io/vgteam/vg:ci-2351-a64b70c1f9345f0821e3f3a600eb8bbf4fe44bf2 \
+    vg gbwt \
+    -x liftover_snp1kg_grch38_nosegdup.xg \
+    -o liftover_snp1kg_grch38_nosegdup.sampled.gbwt \ 
+    -g liftover_snp1kg_grch38_nosegdup.sampled.gg \
+    liftover_snp1kg_grch38_nosegdup.gbwt \
+    -l -n 64 \
+    -t 32 -p
+
+# Build the minimizer index
+docker run \
+-v ${PWD}:${HOME} -w ${HOME} quay.io/vgteam/vg:ci-2351-a64b70c1f9345f0821e3f3a600eb8bbf4fe44bf2 \
+    vg minimizer \
+    -g liftover_snp1kg_grch38_nosegdup.sampled.gbwt \
+    -i liftover_snp1kg_grch38_nosegdup.sampled.trivial_snarls_dist.min \
+    -d liftover_snp1kg_grch38_nosegdup.trivial.snarls.dist \
+    -G liftover_snp1kg_grch38_nosegdup.sampled.gg \
+    -t 32 -p
+
+mv liftover_snp1kg_grch38_nosegdup.gbwt liftover_snp1kg_grch38_nosegdup.old.gbwt
+mv liftover_snp1kg_grch38_nosegdup.sampled.gbwt liftover_snp1kg_grch38_nosegdup.gbwt
+mv liftover_snp1kg_grch38_nosegdup.sampled.trivial_snarls_dist.min liftover_snp1kg_grch38_nosegdup.min
+mv liftover_snp1kg_grch38_nosegdup.sampled.gg liftover_snp1kg_grch38_nosegdup.gg
+mv liftover_snp1kg_grch38_nosegdup.trivial.snarls.dist liftover_snp1kg_grch38_nosegdup.dist
+
